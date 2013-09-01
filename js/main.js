cb28465030f350684a1ffa44ad54b4b6ed392dd9
Prasanna N <prasann87@gmail.com>
2013-09-01 17:35:52 +0530

    row column count available now

--- js/main.js
+++ js/main.js
@@ -2,4 +2,14 @@
   $('table#myTable th').on('click', function(){
     alert($(this).text());
   });
+
+  function prepareTable(table_id){
+    table = $('table' + '#' + table_id);
+    rows = table.find('tbody > tr').size();
+    cols = table.find('thead th').size();
+    console.log("rows: " + rows + ", cols: " + cols);
+  }
+
+  prepareTable('myTable');
+
 })(jQuery);
